e6e3094324111e8213a41072ac21650f95edeaf9
Darko <darko.milovanovic@devoteam.com>
2019-07-03 15:59:10 +0200

    Resolving REGEX

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,12 +14,12 @@ public class Greeter {
   /**
    * Changes the name of this Student.
    * This may involve a lengthy legal process.
-   * @param someone This Student's new name.  
+   * @param someone This Student's new name.
    *                Should include both first
    *                and last name.*/
-  
+
   public final String greet(final String someone) {
-    /** 
+    /**
      * @return greeting This returns a greeting.*/
     return String.format("Hello, %s!", someone);
   }
