b56564cddce9c16c170dfa59bf8fd222b161f4fb
Darko <darko.milovanovic@devoteam.com>
2019-07-03 14:28:38 +0200

    10.change

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,15 +11,16 @@ public class Greeter {
   public Greeter() {
 
   }
-/** @return greeting*/
- /**
+  /**
    * Changes the name of this Student.
    * This may involve a lengthy legal process.
    * @param someone This Student's new name.  
    *                Should include both first
    *                and last name.*/
-
-   public final String greet(final String someone) {
+  
+  public final String greet(final String someone) {
+    /** 
+     * @return greeting This returns a greeting.*/
     return String.format("Hello, %s!", someone);
   }
 }
